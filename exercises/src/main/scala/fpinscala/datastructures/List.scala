a85715eb4c305ec65ca82c2b2577d96f64ea93d0
Sandeep Dhawan <san.dhawan@gmail.com>
2015-07-08 00:09:29 -0400

    completed exercises 3.22

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -1,5 +1,7 @@
 package fpinscala.datastructures
 
+import scala.annotation.tailrec
+
 sealed trait List[+A] // `List` data type, parameterized on a type, `A`
 case object Nil extends List[Nothing] // A `List` data constructor representing the empty list
 /* Another data constructor, representing nonempty lists. Note that `tail` is another `List[A]`,
@@ -7,23 +9,25 @@ which may be `Nil` or another `Cons`.
  */
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
-object List { // `List` companion object. Contains functions for creating and working with lists.
-  def sum(ints: List[Int]): Int = ints match { // A function that uses pattern matching to add up a list of integers
+object List {
+  // `List` companion object. Contains functions for creating and working with lists.
+  def sum(ints: List[Int]): Int = ints match {
+    // A function that uses pattern matching to add up a list of integers
     case Nil => 0 // The sum of the empty list is 0.
-    case Cons(x,xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
+    case Cons(x, xs) => x + sum(xs) // The sum of a list starting with `x` is `x` plus the sum of the rest of the list.
   }
 
   def product(ds: List[Double]): Double = ds match {
     case Nil => 1.0
     case Cons(0.0, _) => 0.0
-    case Cons(x,xs) => x * product(xs)
+    case Cons(x, xs) => x * product(xs)
   }
 
   def apply[A](as: A*): List[A] = // Variadic function syntax
     if (as.isEmpty) Nil
     else Cons(as.head, apply(as.tail: _*))
 
-  val x = List(1,2,3,4,5) match {
+  val x = List(1, 2, 3, 4, 5) match {
     case Cons(x, Cons(2, Cons(4, _))) => x
     case Nil => 42
     case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
@@ -34,17 +38,17 @@ object List { // `List` companion object. Contains functions for creating and wo
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
-      case Cons(h,t) => Cons(h, append(t, a2))
+      case Cons(h, t) => Cons(h, append(t, a2))
     }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = // Utility functions
     as match {
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
     }
 
   def sum2(ns: List[Int]) =
-    foldRight(ns, 0)((x,y) => x + y)
+    foldRight(ns, 0)((x, y) => x + y)
 
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
@@ -52,72 +56,141 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def tail[A](l: List[A]): List[A] = l match {
     case Nil => Nil
-    case Cons(h,t: List[A]) => t
+    case Cons(h, t: List[A]) => t
   }
 
   def setHead[A](l: List[A], h: A): List[A] = l match {
-    case Nil => Cons(h,Nil)
-    case Cons(head,tail) => Cons(h,tail)
+    case Nil => Cons(h, Nil)
+    case Cons(head, tail) => Cons(h, tail)
   }
 
   def drop[A](l: List[A], n: Int): List[A] = l match {
     case Nil => Nil
     case _ if (n == 0) => l
-    case Cons(h,t) => drop(t,n-1)
+    case Cons(h, t) => drop(t, n - 1)
   }
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
-    case Cons(h,t) if (f(h)) => dropWhile(t,f)
+    case Cons(h, t) if (f(h)) => dropWhile(t, f)
     case _ => l
   }
 
   def init[A](l: List[A]): List[A] = l match {
-    case Cons(h,Nil) => Nil
-    case Cons(h,t) => Cons(h,init(t))
+    case Nil => Nil
+    case Cons(h, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
   }
 
-  def length[A](l: List[A]): Int = foldRight(l, 0)((_,acc) => acc + 1)
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, acc) => acc + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
-    @annotation.tailrec
-    def loop(l: List[A],acc: B): B = l match {
-      case Nil => acc
-      case Cons(head:A,tail:List[A]) => loop(tail,f(acc,head))
-    }
-    loop(l,z)
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, (f(z, h)))(f)
   }
 
-  def sum3(l:List[Int]) =
-      foldLeft(l,0)((x,y)=> x + y)
+  def sum3(l: List[Int]) =
+    foldLeft(l, 0)((x, y) => x + y)
 
   def product3(l: List[Double]) =
-      foldLeft(l,1.0)(multi)
-    //foldLeft(l,1.0)((x,y)=> x * y)
+    foldLeft(l, 1.0)(multi)
 
-   def multi(x:Double, y:Double) = y
+  //foldLeft(l,1.0)((x,y)=> x * y)
 
-  def length2[A](l: List[A]): Int = foldLeft(l,0)((acc,_) => acc + 1)
+  def multi(x: Double, y: Double) = y
 
-  def reverseList[A](l: List[A]): List[A]= {
+  def length2[A](l: List[A]): Int = foldLeft(l, 0)((acc, _) => acc + 1)
 
-    def loop(l:List[A]): List[A] = l match {
-      case Cons(h,Nil) => l
-      case Cons(h,t) => setTail(h,loop(t))
+  def reverseList[A](l: List[A]): List[A] = {
+
+    def loop(l: List[A]): List[A] = l match {
+      case Cons(h, Nil) => l
+      case Cons(h, t) => setTail(h, loop(t))
       case Nil => Nil
     }
     loop(l)
   }
 
-  def reverseList2[A](l: List[A]): List[A]= {
-    foldRight(l,Nil)(setTail)
-    //???
+  def reverseList2[A](l: List[A]): List[A] = {
+    // foldRight(l,Nil)(setTail)
+    ???
   }
 
-  def setTail[A](n:List[A],l:List[A]): List[A] = l match {
-    //case Nil => Cons(n,Nil)
-    case Cons(h,t) => Cons(h,setTail(n,t))
+  def setTail[A](n: A, l: List[A]): List[A] = l match {
+    case Nil => Cons(n, Nil)
+    case Cons(h, t) => Cons(h, setTail(n, t))
+  }
+
+  def concat[A](l1: List[List[A]], l2: List[List[A]]): List[A] = {
+    append(foldLeft(l1, List[A]())(append),
+      foldLeft(l2, List[A]())(append))
+  }
+
+  def append2[A](l1: List[A], l2: List[A]): List[A] = foldLeft(l2, l1)((b, a) => setTail(a, b))
+
+  //def addOne(l:List[Int]): List[Int] = foldLeft(l,List[Int]())((b,a)=> setTail(a+1,b))
+  def addOne(l: List[Int]): List[Int] = foldRight(l, List[Int]())((b, a) => Cons(b + 1, a))
+
+  def doubleToString(l: List[Double]): List[String] = foldRight(l, List[String]())((b, a) => Cons(b.toString, a))
+
+  def map[A, B](l: List[A])(f: A => B): List[B] = foldRight(l, List[B]())((b, a) => Cons(f(b), a))
+
+  def flatMap2[A,B](as: List[A])(f: A => List[B]): List[B] = as match {
+    case Nil => List[B]()
+    case Cons(h,t) => append(f(h),flatMap2(t)(f))
+  }
+
+  def flatMap[A,B](as: List[A])(f: A => List[B]): List[B] = foldLeft(as,List[B]())((b,a) => append(b,f(a)))
+
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
+    //  case Nil => Nil
+    // case Cons(h,t) if (f(h)) => filter()
+  ???
+  }
+
+  def func[A](h: A,f:A => Boolean, list: List[A]): List[A] = {
+    if (f(h)) Cons(h,list)
+    else list
+  }
+
+  def remove[A](as: List[A],f:A => Boolean): List[A] = as match {
+    case Nil => List[A]()
+    case Cons(h,t) => func(h,f,remove(t,f))
+  }
+
+  def remove2[A](as: List[A],f:A => Boolean): List[A] = {
+
+    @tailrec
+    def loop(as:List[A],acc:List[A],f:A => Boolean): List[A] = as match {
+      case Nil => acc
+      case Cons(h,t) if (f(h)) => loop(t,setTail(h,acc),f)
+      case Cons(h,t) => loop(t,acc,f)
+    }
+    loop(as,Nil,f)
+  }
+
+  def remove3[A](as:List[A],f: A=> Boolean): List[A] = {
+    flatMap(as)((a)=> if (f(a)) Cons(a,Nil) else List[A]())
+  }
+
+  @tailrec
+  def getelementatpos(as:List[Int],pos: Int): Int = as match {
+    case _ if (pos > length(as)-1) => -1
+    case Cons(h,_) if (pos == 0) => h
+    case Cons(h,t) => getelementatpos(t,pos-1)
+  }
+
+  def addList(l1: List[Int], l2:List[Int]): List[Int] = {
+    @tailrec
+    def loop (l1:List[Int],l2:List[Int],n2:Int,acc: List[Int],pos: Int): List[Int] = l1 match {
+      case Nil => acc
+      case Cons(h,t) if (n2 == -1) => append(acc,l1)
+      case Cons(h,t) => loop(t,l2,getelementatpos(l2,pos+1),setTail(h+n2,acc),pos+1)
+    }
+
+    if (length(l1)> length(l2)) loop(l1,l2,getelementatpos(l2,0),List[Int](),0)
+    else loop(l2,l1,getelementatpos(l1,0),List[Int](),0)
   }
 
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
-}
+ }
